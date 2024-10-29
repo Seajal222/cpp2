e2d25ddbcc48d0051f2041705f8c5e646c71d3d4
Seajal222 <seajaluniyal22@gmail.com>
2024-10-29 23:22:31 +0530

    Create inline function to display sum.cpp

---
+++ inline function to diaplay sum.cpp
@@ -0,0 +1,17 @@
+//create an inline function  in student class to display the sum of two numbers.
+#include <iostream>
+using namespace std;
+class Student {
+public:
+    inline void displaySum(int a, int b) {
+        cout << "The sum of " << a << " and " << b << " is " << (a + b) << endl;
+    }
+};
+int main() {
+    Student student;
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    student.displaySum(num1, num2);
+    return 0;
+}
